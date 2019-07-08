c100c156893fc1929e4c7be03c9345ee18735f64
avni jain <javni647@gmail.com>
2019-07-08 19:35:41 +0530

    basic

---
+++ assets/sketch.js
@@ -0,0 +1,58 @@
+var xdiff = 6;
+var x = 50;
+var y =30;
+var x1 = x++;
+var aaa = document.getElementById('container').clientW;
+var rows = aaa/50;
+// var y1 = x++;
+
+function setup() {
+  var clientW = document.getElementById('sketch-holder').offsetWidth;
+
+  // var canvas = createCanvas(940, 480);
+  // canvas.parent('sketch-holder');
+  resizeCanvas(940, 480);
+
+  // strokeWeight(1);
+  // strokeJoin(ROUND);
+  //   fill (240);
+  //   rect(50, 25, 800, 350);
+  //   for(var i = 56; i<800; i+=56){
+  //     fill(50);
+  //     rect(i, 31, x, y);
+  //   }
+  //   for(var i = 56; i<800; i+=56){
+  //     fill(50);
+  //     rect(i, 70, x, x);
+  //   }
+  //   for(var i = 56; i<800; i+=56){
+  //     fill(50);
+  //     rect(i, 130, x, x);
+  //   }
+  //   for(var i = 56; i<800; i+=56){
+  //     fill(50);
+  //     rect(i, 190, x, x);
+  //   }
+  //   for(var i = 56; i<800; i+=56){
+  //     fill(50);
+  //     rect(i, 250, x, x);
+  //   }
+  //   for(var i = 56; i<800; i+=56){
+  //     fill(50);
+  //     rect(i, 310, x, x);
+  //   }
+}
+// function setup() {
+//   createCanvas(940, 480);
+//   // noStroke();
+//   background(255);
+// }
+function draw() {
+  //background(10, 10, 10, 10);
+
+}
+
+function mouseClicked() {
+
+
+}
