af9e5d2e954100e4b11b2abe26faccb83c8aeedf
Salil <salil458@gmail.com>
2019-07-19 17:09:13 +0530

    another one

--- assets/sketch.js
+++ assets/sketch.js
@@ -1,4 +1,4 @@
-var mic;
+/*var mic;
 function setup(){
     var clientW = document.getElementById('sketch-holder').clientWidth;
     var clientH = document.getElementById('sketch-holder').clientHeight;
@@ -19,45 +19,38 @@ function draw(){
 
     }
 }
-
-
-/*let mic, fft;
-var kuler;
-
+*/
+let mic;
+var f;
+var Fs, Fc, Fe;
 var incr = 0.002;
 var start = 0;
 var t = 0;
-
 function setup() {
-   // var clientW = document.getElementById('sketch-holder').clientWidth;
-   // var clientH = document.getElementById('sketch-holder').clientHeight;
-   var canvas = createCanvas(600, 600);
-   // canvas.parent('sketch-holder');
-   // console.log(clientW + " " + clientH);
-   background(200);
-   mic = new p5.AudioIn();
-   mic.start();
-   fft = new p5.FFT();
-   fft.setInput(mic);
-   noFill();
+var clientW = document.getElementById('sketch-holder').clientWidth;
+    var clientH = document.getElementById('sketch-holder').clientHeight;
+    var canvas = createCanvas(clientW, clientH);
+    canvas.parent('sketch-holder');  
+  //cnv = createCanvas(600, 600);
+ mic = new p5.AudioIn()
+    mic.start();
+    fft = new p5.FFT();
+    fft.setInput(mic); 
 }
 
 function draw() {
-   background(0);
-   let spectrum = fft.analyze();
-   stroke(200);
+  background(0);
+  var waveform = fft.waveform();
+  
+  translate(600/2, 600/2);
 
-}
-
-
-translate(600/2, 600/2);
-
-for (i = 0; i < spectrum.length; i++) {
-   var c = spectrum[i]*1;
-   var Fc = map(c, 0, 400, 255, 0);
-   var Fe = map(c, 0, 400, 2, 15);
-   var Fs = map(c, 0, 400, 0, 0.000019); //0.00008 for the melody 0.00002 for the audio
-   rotate(sin(t)+Fs);
+for (i = 0; i < waveform.length; i++) {
+   f = waveform[i];
+    c = waveform[i]*10;
+    Fc = map(c, -1, 1, 255, 0);
+    Fe = map(c, -1, 1, 0.1, 5);
+    Fs = map(c, -1, 1, -0.00001, 0.00000008); //0.00008 for the melody 0.00002 for the audio
+   rotate(cos(t)+Fs);
    scale(1.01);
    stroke(Fc);
    strokeWeight(Fe);
@@ -65,17 +58,9 @@ for (i = 0; i < spectrum.length; i++) {
    t += Fs;
 }
 
+    console.log(f*10);
+
+
 start += incr;
 t += incr;
-
-
-//    
-//		 beginShape();
-//  for (i = 0; i < spectrum.length; i++) {
-//    vertex(i, map(spectrum[i], 0, 255, 600, 0));
-//
-//  }
-//  endShape();
-//
-//}
-*/
+}
